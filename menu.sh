f5e50bb1f60556f4d50f01d6f6b7d0224a4bb598
gastonCh <gaston.ch.esp@gmail.com>
2020-11-09 23:02:35 -0300

    Add files via upload

---
+++ menu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+clear
+while :
+do
+    echo " Escoja una opcion "
+    echo "1. Alta Usuario"
+    echo "2. Baja Usuario"
+    echo "3. Modificacion de Usuario?"
+    echo "4. Salir"
+    echo -n "Seleccione una opcion [1 - 4]"
+    read opcion
+    case $opcion in
+        1) echo " Ingrese un nombre de usuario";
+        read nombre;
+        useradd -m $nombre;
+        echo "El usuario $nombre se dio de alta";;
+        2) echo "Ingrese el nombre del usuario que quiere dar de baja";
+        read nombre;
+        userdel -r $nombre;
+        echo "El usuario $nombre se dio de baja";;
+        3) echo "Ingrese el nombre de usuario que desea modificar";
+        read nombre;
+        sudo passwd $nombre
+        echo "Al usuario $nombre se le ha modificar la clave";;
+        4) echo "Fin";
+        exit 1;;
+        *) echo "$opcion es una opcion invalida";
+        echo "Presiona una tecla para continuar...";
+        read foo;;
+    esac
+done
